8f949f2f5471294e781ace3dee4bbe5b1a730551
Daniel Kim <danieljkim1017@gmail.com>
2025-08-07 01:48:05 -0700

    more saving and listing domain work

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,24 @@
 import DomainRow from "@/components/DomainRow";
 import DoubleHeader from "@/components/DoubleHeader";
 import NewDomainForm from "@/components/NewDomainForm";
+import { getServerSession } from "next-auth";
 import { Span } from "next/dist/trace";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { Domain } from "@/models/domain";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  const user = session.user;
+  const domains = await Domain.find({owner:user.email});
   return (
     <div>
       <NewDomainForm />
-      <DoubleHeader preTitle={'Your Domains'} mainTitle={'4 Domains'} />
-      <DomainRow/>
-      <DomainRow/>
-      <DomainRow/>
-      <DomainRow/>
+      <DoubleHeader
+        preTitle={'Your Domains'} 
+        mainTitle={domains.length+' Domains'} />
+      {domains.map(domain => (
+         <DomainRow {...domain.toJSON()} />
+      ))}
     </div>
   );
-}
+} 
--- src/components/DomainRow.js
+++ src/components/DomainRow.js
@@ -1,10 +1,10 @@
-export default function DomainRow() {
+export default function DomainRow({owner, domain, icon}) {
     const keywords = ['github', 'git', 'copilot', 'git share code', 'free git repos',];
     return (
         <div className="flex gap-2 bg-white border border-blue-200 border-b-4 p-4 rounded-lg items-center my-3">
-          <img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" className="h-12"/>
+          <img src={icon} className="h-12"/>
           <div>
-            <h2 className="font-bold text-xl leading-3">Github.com</h2>
+            <h2 className="font-bold text-xl leading-4">{domain}</h2>
             {keywords.map(keyword => (
               <>
                 <span className="text-xs text-gray-500 bg-slate-100 rounded-md p-1 mt-1 inline-block">
